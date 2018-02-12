8b73af3c212dc904cb1f6cff846ddc8fb1e95109
jb26444 <jan.blahuta@gmail.com>
2018-02-12 12:10:26 +0000

    Delete host_config_file.py

--- CLI Automation v1/host_config_file.py
+++
@@ -1,7 +0,0 @@
-#
-#
-# add your configurations in a list format
-#
-#host_conf = ['show license usage', 'show version', 'show modul', 'show inventory', 'show module fex',]
-#host_conf_1 = ['sh run | inc ntp','sh ntp status','sh run | inc tacacs']
-host_conf = open ("conf.txt").readlines() 
