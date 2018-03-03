c117cd62e3f2950c24ae6745ce3e7183b2863a4c
jb26444 <jan.blahuta@gmail.com>
2018-03-03 23:11:38 +0100

    Win7

--- Python/SCRIPTS/Compare/compare.py
+++ Python/SCRIPTS/Compare/compare.py
@@ -2,12 +2,12 @@ import sys
 
 
 word = "#"
-f = open(base.txt)
+f = open("base.txt", "r+")
 d = f.readlines()
 f.seek(0)
 for line in d:
-    if word in d
-       print d.split(word)
+    if word in d:
+       print (d.split(word))
        f.write(d)
 f.truncate()
 f.close()
