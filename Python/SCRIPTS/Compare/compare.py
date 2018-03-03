9aa44abdbfbc708a0589eacf8e45c789682f5f8d
jb26444 <jan.blahuta@gmail.com>
2018-03-03 21:16:57 +0100

    Win7

---
+++ Python/SCRIPTS/Compare/compare.py
@@ -0,0 +1,13 @@
+import sys
+
+
+word = "#"
+f = open(base.txt)
+d = f.readlines()
+f.seek(0)
+for line in d:
+    if word in d
+       print d.split(word)
+       f.write(d)
+f.truncate()
+f.close()
